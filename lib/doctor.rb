5f57129f78fa466298224cc588537aa8021620cd
Kwane Smith <smithkwane6@gmail.com>
2020-02-20 15:46:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,20 @@
 class Doctor
-
   attr_accessor :name
-  @@all = []
+
+    @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+
   end
 
   def appointments
@@ -13,15 +22,8 @@ class Doctor
   end
 
   def patients
-    appointments.map {|appointment| appointment.patient}
+    appointments.collect {|appointment| appointment.patient}
   end
 
-  def self.all
-    @@all
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, date, self)
-  end
 
 end
